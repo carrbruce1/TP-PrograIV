54255809a90b3bf0c1903134e472f0bbbde04ac5
carrbruce1 <brucecarr1290@hotmail.com>
2026-05-12 12:21:00 -0300

    Sprint 2

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+  
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+
+  async signIn(email: string, pass: string) {
+    return await this.supabase.auth.signInWithPassword({
+      email: email,
+      password: pass,
+    });
+  }
+
+
+  async signUp(email: string, pass: string, metadata: any) {
+    return await this.supabase.auth.signUp({
+      email: email,
+      password: pass,
+      options: {
+        data: metadata 
+      }
+    });
+  }
+
+  
+  async signOut() {
+    const { error } = await this.supabase.auth.signOut();
+    return error;
+  }
+
+ 
+  async getUser() {
+    const { data: { user } } = await this.supabase.auth.getUser();
+    return user;
+  }
+
+  
+
+
+}
+
